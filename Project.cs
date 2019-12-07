03f2c6bb963bd591eee4702e867c3c7b1ce57c60
Byron Lee <byronn.lee@gmail.com>
2019-12-07 09:56:53 +0000

    added a message to the exception object

--- Project.cs
+++ Project.cs
@@ -36,9 +36,9 @@ namespace TreehouseDefense
   try{
     MapLocation maplocation = new MapLocation(20, 20, map);
   }
-  catch(Exception)
+  catch(Exception ex)
   {
-      Console.WriteLine("That map location is not on the map!");
+      Console.WriteLine(ex.Message);
   }
     }
   }
