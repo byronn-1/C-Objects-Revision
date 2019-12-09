4eb3bfe0c19439874a8fc2bd65fb6c12c62bf4a5
Byron Lee <byronn.lee@gmail.com>
2019-12-09 13:14:26 +0000

    added more properties and methods to the invader class

--- Invader.cs
+++ Invader.cs
@@ -27,9 +27,37 @@ namespace TreehouseDefense
           _location = value;
           // System.Console.WriteLine("location changed");
         } */
-        public MapLocation Location { get; private set;}
-      }
+
+  private readonly Path _path;
+  private int _pathStep = 0;
+  public int _health = 100;
+  public MapLocation Location =>  _path.GetLocationAt(_pathStep);
+
+  public Invader (Path path)
+  {
+    _path = path;
+  }
+
+  public void Move() => _pathStep += 1;
+
+
+// invader health ......
+// variable to store how much health the invader has 
+// invader has to start out with an amount of health
+// tower class to decrease the health of the invader IF (they succsesfully hit it)
+
+    public int Health { get; private set; } = 2;
+      
+    public void DecreaseHealth(int factor)
+    {
+      Health -= factor;
+    }
+
+    public bool HasScored {get{ return _pathStep >= _path.Length; } };
+    
+    public bool IsNeutralized => Health <= 0;
+
+    public bool IsActive => !(IsNeutralized || HasScored);
+  }
 }
-
-
 //Team_Treehouse_C__Objects
